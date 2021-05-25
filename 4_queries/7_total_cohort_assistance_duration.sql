a60d06b39475b40f8efedf186999a6eeab604f22
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-25 14:26:25 -0400

    add queries 4

---
+++ 4_queries/7_total_cohort_assistance_duration.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, sum(completed_at - started_at) as total_duration
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_duration
+;
